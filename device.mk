dc529a9fd5bd172d0c79d7847eb27083d780d089
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 22:15:24 +0300

    pdx201: Don't build tinymix
    * This tool can be built manually for debugging cases,
      there is no need to ship it in production
    
    Change-Id: I9ab8b076b4bede70c368c198960ceabc68dad65b

--- device.mk
+++ device.mk
@@ -88,8 +88,7 @@ PRODUCT_PACKAGES += \
     libspkrprot \
     libssrec \
     libvisualizer \
-    libvolumelistener \
-    tinymix
+    libvolumelistener
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
