e8e9c405c336b55bceba7158caa58106158b9cc4
Nicholas Ambur <nambur@google.com>
2020-03-02 15:05:49 -0800

    pdx201: add SoundTrigger HAL v2.3 support
    Bug: 139071862
    Test: gts-tradefed run gts-dev -m GtsAssistIntentTestCases
    Change-Id: I2d17aade762ceb7800d5305119c37ec0f5991f35

--- device.mk
+++ device.mk
@@ -59,7 +59,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     audio_amplifier.trinket \
