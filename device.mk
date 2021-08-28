6bd3dd05c7ba37cfab799c9e7f0c2d7a4add8925
sjllls <2012ji123@gmail.com>
2021-08-28 12:35:47 +0800

    pdx201: Build livedisplay

--- device.mk
+++ device.mk
@@ -308,6 +308,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.pdx201
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
