474ab59a80e850dba0783db2647126a31fb5af59
althafvly <althafvly@gmail.com>
2022-02-26 22:16:30 +0530

    pdx201: Set BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE
    Change-Id: I108504e650895ed62dd54f30d6c53f9d16baa5a3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,6 +153,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
