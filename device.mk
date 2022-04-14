1cae2fb32228d93d95b027768067106c0fcf33be
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 23:46:09 -0700

    pdx201: Drop all component overrides
     * Qualcomm no longer disables these for targets
       that support RRO, e.g. all QSSI targets.
    
    Change-Id: I0372817623e8d0e58926fbfb519671c3caa07b86

--- device.mk
+++ device.mk
@@ -170,10 +170,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.semc.hardware.charger@1.0.vendor
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
