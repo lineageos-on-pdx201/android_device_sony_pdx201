f542f962062db36c830e7aba37a91035d8d6b7da
Shashi Shekar Shankar <quic_ssbang@quicinc.com>
2022-09-02 09:35:06 +0530

    pdx201: init: Update copyright markings
    Update copyright markings in post-boot.
    
    Change-Id: If43fb4aa47a1d0c8cfe87df376f1002854b0480f
    CRs-Fixed: 3281988

--- init/init.qcom.post_boot.sh
+++ init/init.qcom.post_boot.sh
@@ -26,6 +26,10 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+# Changes from Qualcomm Innovation Center are provided under the following license:
+# Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+#
 
 function 8953_sched_dcvs_eas()
 {
