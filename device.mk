630125c5b6e1c398eb62ff0a90c668d082b3b4f7
Luofan Chen <clfbbn@gmail.com>
2022-10-07 23:21:35 +0200

    pdx201: Switch to sony LiveDisplay impl

--- device.mk
+++ device.mk
@@ -318,7 +318,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.1-service.sony
 
 # Media
 PRODUCT_PACKAGES += \
