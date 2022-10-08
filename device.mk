4b582eff38ac15ad68dbb17aa3cea6eb727f6332
TheMalachite <eliasgheeraert@gmail.com>
2022-10-08 21:18:36 +0200

    fixup! pdx201: Fixup vibrator input config path

--- device.mk
+++ device.mk
@@ -488,7 +488,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensouce/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_PACKAGES += \
