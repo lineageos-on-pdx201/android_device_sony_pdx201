67b5e35da7f4353412aaff785d67767f4d15e781
TheMalachite <eliasgheeraert@gmail.com>
2022-11-01 00:27:41 +0100

    pdx201: Initial SEPolicy rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,8 +163,10 @@ TARGET_USERIMAGES_USE_F2FS := true
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
-# Sepolicy
+# SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
