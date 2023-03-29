7930564149e9cc65df9235e35beec98a339bb0cc
Michael W <baddaemon87@gmail.com>
2023-03-29 21:15:23 +0800

    pdx201: init: Simplify command that checks model
    Change-Id: I3608cd42335da3b93a7a7b0ecbf82e720ebcd27f

--- init/init.qcom.msim.sh
+++ init/init.qcom.msim.sh
@@ -1,6 +1,6 @@
 #!/vendor/bin/sh
 
-model=`grep -am1 '[Mm][Oo][Dd][Ee][Ll]:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*[Mm][Oo][Dd][Ee][Ll]:[ ]*\([A-Za-z0-9-]*\).*$/\1/'` 2> /dev/null
+model=`grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
 
 case "$model" in
     "XQ-AU42" | "XQ-AU52" )
