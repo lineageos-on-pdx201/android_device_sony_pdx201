11c2eff349946574667cbf7098ba1580e95502db
Mashopy <eliasgheeraert@gmail.com>
2023-05-01 12:53:02 +0200

    pdx201: Drop dupplicated build targets
    Change-Id: Ic656d8cce67c0d15137b86d53f1e5d3add381d4f

--- device.mk
+++ device.mk
@@ -125,13 +125,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
-
-PRODUCT_PACKAGES += \
-    audio.bluetooth.default
 
 PRODUCT_PACKAGES += \
     libbthost_if
@@ -202,7 +198,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator@1.0.vendor \
     vendor.qti.hardware.display.composer@1.0.vendor \
     vendor.qti.hardware.display.composer@2.0.vendor \
-    vendor.qti.hardware.display.mapper@1.0.vendor \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
