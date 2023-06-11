c0f82dd6f1f0ca34d80d37cb7fb91e0d85bc99f3
Mashopy <eliasgheeraert@gmail.com>
2023-06-11 21:56:57 +0200

    pdx201: init: Set ro.boot.product.hardware.sku to LTALabel model
    Change-Id: I50e4228f455e018b47cc603e103298657d7af133

--- init/init.sony.sh
+++ init/init.sony.sh
@@ -2,6 +2,17 @@
 
 model=`grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
 
+# SKU
+case "$model" in
+    "XQ-AU42" )
+        setprop ro.vendor.somc.model XQ-AU42;;
+    "XQ-AU51" )
+        setprop ro.vendor.somc.model XQ-AU51;;
+    "XQ-AU52" )
+        setprop ro.vendor.somc.model XQ-AU52;;
+esac
+
+# Radio
 case "$model" in
     "XQ-AU42" | "XQ-AU52" )
         setprop vendor.radio.multisim.config dsds;;
