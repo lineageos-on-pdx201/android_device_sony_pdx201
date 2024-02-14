0609ee4948a9a589020a8faabb07f74ac8646608
Mashopy <eliasgheeraert@gmail.com>
2024-02-14 16:08:02 +0100

    pdx201: Inherit usb component

--- device.mk
+++ device.mk
@@ -252,7 +252,8 @@ TARGET_COMMON_QTI_COMPONENTS := \
     media \
     overlay \
     perf \
-    telephony
+    telephony \
+    usb
 
 # QMI
 PRODUCT_PACKAGES += \
@@ -291,16 +292,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
-
-# USB
-$(call inherit-product, vendor/qcom/opensource/usb/vendor_product.mk)
-
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Userdata
 PRODUCT_FS_COMPRESSION := 1
