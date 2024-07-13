441f31f90a47c5c401240d20d091c5682ed00350
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    pdx201: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- device.mk
+++ device.mk
@@ -185,8 +185,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     vendor.display.config@1.11.vendor \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
@@ -197,12 +195,12 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.memtrack-service \
     vendor.semc.hardware.display@2.2.vendor
 
 PRODUCT_PACKAGES += \
     gralloc.trinket \
-    hwcomposer.trinket \
-    memtrack.trinket
+    hwcomposer.trinket
 
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
