2403d904205d36021f39e0631929c7bf7478d1e7
Michael Bestas <mkbestas@lineageos.org>
2025-01-02 01:12:07 +0200

    pdx201: Disable OMX service
    Change-Id: I5381e098af64271f9d71c484262e1710e8fcacea

--- lineage_pdx201.mk
+++ lineage_pdx201.mk
@@ -6,6 +6,7 @@
 
 # Inherit framework first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from pdx201 device
