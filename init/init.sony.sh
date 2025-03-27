59cc971812e007e488cbecda73ecd6ed40bfab2c
kyasu <kyasu.dev@gmail.com>
2025-03-27 09:08:51 +0900

    pdx201: Set ro.boot.product.hardware.sku during early-init
    Change-Id: Ic63e456436075128a9019dce9760795ca1c1fe74

--- init/init.sony.sh
+++ init/init.sony.sh
@@ -1,11 +1,11 @@
 #!/vendor/bin/sh
 
-model=`grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
+model=`grep -aim1 'model:' /dev/block/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
 
 # Radio
 case "$model" in
     "XQ-AU52" )
-        setprop vendor.radio.multisim.config dsds;;
+        setprop vendor.radio.hardware.sku ds;;
     * )
-        setprop vendor.radio.multisim.config ss;;
+        setprop vendor.radio.hardware.sku ss;;
 esac
